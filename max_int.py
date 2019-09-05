fa3158245f4901819c31300f116a8d65c23ef22e
Matthildur <matthildur@gestur-277.ru.is>
2019-09-05 10:07:12 +0000

    Initial commit

---
+++ max_int.py
@@ -0,0 +1,6 @@
+#Get an integer in input
+#declare max_int to 0
+#check to see if the input is a negative number, 
+# then the loop will end and print out max_int
+##compare the input to the max_int to see if is is higher, 
+# if it is higher then it will get the value of the input
