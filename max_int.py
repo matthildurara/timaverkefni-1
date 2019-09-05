84d7016158abcc6cea4d1da13e3e7da648e3fc2c
Matthildur <matthildur@gestur-277.ru.is>
2019-09-05 10:13:19 +0000

    created max_int

--- max_int.py
+++ max_int.py
@@ -4,3 +4,12 @@
 # then the loop will end and print out max_int
 ##compare the input to the max_int to see if is is higher, 
 # if it is higher then it will get the value of the input
+
+number = int(input("Enter an input: "))
+max_int = 0
+
+while number > 0:
+    if number > max_int:
+        max_int = number
+        number = int(input("Enter an input: "))
+print(max_int)
